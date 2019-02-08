5db27ab16c019568a6a6667859e04af18e9c8762
the_maker <fshmorgilov@gmail.com>
2019-02-08 12:59:06 +0300

    DaoFinished

--- app/src/main/java/com/themaker/fshmo/klassikaplus/data/persistence/dao/PhotoDao.java
+++ app/src/main/java/com/themaker/fshmo/klassikaplus/data/persistence/dao/PhotoDao.java
@@ -1,5 +1,29 @@
 package com.themaker.fshmo.klassikaplus.data.persistence.dao;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import com.themaker.fshmo.klassikaplus.data.persistence.model.DbPhoto;
+import io.reactivex.Single;
 
+import java.util.List;
+
+@Dao
 public interface PhotoDao {
+
+    @Query("SELECT * FROM PHOTOS")
+    Single<List<DbPhoto>> getAll();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(List<DbPhoto> items);
+
+    @Query("DELETE FROM PHOTOS")
+    void deleteAll();
+
+    @Query("SELECT * FROM PHOTOS WHERE id in (:id)")
+    Single<DbPhoto> findById(String id);
+
+    @Query("SELECT * FROM PHOTOS WHERE itemId in (:itemId)")
+    Single<List<DbPhoto>> findByItemId(String itemId);
 }
