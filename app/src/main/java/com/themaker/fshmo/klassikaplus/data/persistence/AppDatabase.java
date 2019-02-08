c2ad8ee5ae102d52f54122c35b62ae936c82aafa
the_maker <fshmorgilov@gmail.com>
2019-02-08 13:04:27 +0300

    app database dao

--- app/src/main/java/com/themaker/fshmo/klassikaplus/data/persistence/AppDatabase.java
+++ app/src/main/java/com/themaker/fshmo/klassikaplus/data/persistence/AppDatabase.java
@@ -6,13 +6,16 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import com.themaker.fshmo.klassikaplus.data.persistence.dao.ItemDao;
+import com.themaker.fshmo.klassikaplus.data.persistence.dao.PhotoDao;
 import com.themaker.fshmo.klassikaplus.data.persistence.model.DbItem;
 
 @Database(entities = {
         DbItem.class
 }, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
+
     public abstract ItemDao itemDao();
+    public abstract PhotoDao photoDao();
 
     private static final String DATABASE_NAME = "klassikaplus.db";
 
