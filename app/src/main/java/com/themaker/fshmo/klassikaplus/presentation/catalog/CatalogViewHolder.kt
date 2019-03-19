bb54a5dccdbe14b277d738ed94ebb1fa08d7f806
Fedor <fshmorgilov@gmail.com>
2019-03-19 13:39:34 +0300

    CatalogViewHolder finished, needs butterknife

--- app/src/main/java/com/themaker/fshmo/klassikaplus/presentation/catalog/CatalogViewHolder.kt
+++ app/src/main/java/com/themaker/fshmo/klassikaplus/presentation/catalog/CatalogViewHolder.kt
@@ -1,5 +1,37 @@
 package com.themaker.fshmo.klassikaplus.presentation.catalog
 
-class CatalogViewHolder {
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import butterknife.ButterKnife
+import com.bumptech.glide.RequestManager
+import com.themaker.fshmo.klassikaplus.data.domain.Item
 
+class CatalogViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+    private val TAG: String = CatalogViewHolder::class.java.name
+
+    private val view = v
+    private lateinit var image: ImageView
+    private lateinit var name: TextView
+    private lateinit var price: TextView
+
+    init {
+        ButterKnife.bind(this, v)
+    }
+
+    fun bind(
+        item: Item,
+        glide: RequestManager,
+        onItemClickListener: CatalogAdapter.OnItemClickListener
+    ) {
+        price.text = item.price?.toString()
+        name.text = item.name
+        item.icon.let { glide.load(it).into(image) }
+        view.setOnClickListener { onItemClickListener.onItemClick(item) }
+        Log.d(TAG, "Binded item: ${item.id}")
+    }
 }
+
