377c26650c7cc3106ef6650242e01e3f84d79d10
Fedor <fshmorgilov@gmail.com>
2019-03-21 05:23:47 +0300

    drawer somewhere done

--- app/src/main/java/com/themaker/fshmo/klassikaplus/presentation/root/MainActivity.java
+++ app/src/main/java/com/themaker/fshmo/klassikaplus/presentation/root/MainActivity.java
@@ -5,15 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.google.android.material.navigation.NavigationView;
 import com.themaker.fshmo.klassikaplus.App;
 import com.themaker.fshmo.klassikaplus.R;
 import com.themaker.fshmo.klassikaplus.data.domain.Item;
 import com.themaker.fshmo.klassikaplus.data.preferences.Preferences;
 import com.themaker.fshmo.klassikaplus.presentation.base.MvpAppCompatActivity;
 import com.themaker.fshmo.klassikaplus.presentation.catalog.CatalogFragment;
-import com.themaker.fshmo.klassikaplus.presentation.novelties.NoveltyFragment;
 import com.themaker.fshmo.klassikaplus.presentation.web_item.WebItemFragment;
 
 import javax.inject.Inject;
@@ -25,6 +26,7 @@ public class MainActivity extends MvpAppCompatActivity implements MainActivityVi
 
     @Inject
     Preferences preferences;
+
 
     @InjectPresenter
     MainActivityPresenter presenter;
@@ -44,6 +46,14 @@ public class MainActivity extends MvpAppCompatActivity implements MainActivityVi
         App.getInstance().getComponent().inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        DrawerLayout drawerLayout = findViewById(R.id.main_base_view_group);
+        NavigationView navigationView = findViewById(R.id.main_navigation);
+        navigationView.setNavigationItemSelectedListener((menuItem) ->{
+            menuItem.setChecked(true);
+            drawerLayout.closeDrawers();
+            return true;
+        });
+
         preferences.setFirstTimeAppLaunch();
 //        getSupportFragmentManager().beginTransaction()
 //                .replace(R.id.main_frame, new NoveltyFragment())
